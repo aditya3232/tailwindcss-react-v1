6bf7c3139b0dd91677fab118f6d7173a1d5b05fe
aditya3232 <m.aditya3232@gmail.com>
2021-04-30 10:53:09 +0700

    Membuat persiapan untuk komponen, pages, images
    - buat folder components didalam folder src
    - buat folder images didalam folder src
    - buat folder pages didalam folder src
    - komponen yang dibuat adalah (Navbar.js, Hero.js, Footer.js, Dropdown.js, Content.js)
    - semua komponen diimport di App.js
    - komponenen Navbar.js berisi Link dari react-router-dom. (nanti link akan menuju ke pages)
    - di index.js import BrowserRouter
    - di App.js div nya diganti fragment saja
    - jangan lupa install "npm install --save react-router-dom"

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+// gunakan snippet rafce
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/">EGG</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
