0eb8a1daeafbc02adc722d8ba165bdbbeafb433e
aditya3232 <m.aditya3232@gmail.com>
2021-05-05 10:36:21 +0700

    penambahan komentar ke-2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,8 @@ const Navbar = ({ toggle }) => {
       {/* icon open dropdown menu. diicon ini ada handler onClik yg memanggil fungsi toggle */}
       {/* svg hanya akan muncul ketika layar dibawah md, jika md atau diatasnya maka hidden */}
       {/* ketika svg diklik maka akan menampilkan dropdown, */}
-      {/* karena variabel toggle akan menampilkan dropdown, dan ketika diklik lagi menyembunyikannya  */}
+      {/* karena variabel toggle berisi isOpen, yang mana isOpen bernilai true ketika diklik, */}
+      {/* sehingga akan menampilkan dropdown */}
       <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
